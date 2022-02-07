f5404a7201938f323ba226aa24dccf93fe3c5d13
Luke Coulton <luke@luke.org.uk>
2022-02-07 18:59:51 +0000

    Created project

---
+++ src/riderlambdatest/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace riderlambdatest.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return Ok();
+        } 
+    }
+
+    [Route("/[controller]")]
+    public class HomeAgainController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        } 
+    }
+}
